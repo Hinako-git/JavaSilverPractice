cdcf8341a9f1e7a3b192b3c197746856ee085a28
Hina <yhz26116@gmail.com>
2024-09-08 00:27:29 +0900

    修正

---
+++ src/Question3_13.java
@@ -0,0 +1,9 @@
+public class Question3_13 {
+    public static void main (String[] args){
+        String a = new String("sample");
+        String b = "sample";
+        System.out.println(a == b);
+        System.out.println(",");
+        System.out.println(a.equals(b));
+    }
+}
